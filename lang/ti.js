3a048826febad0c1cd391ccf1fd376fd2735a840
hopeingoderi <merry.grace44@yahoo.com>
2026-03-07 03:44:05 +0100

    eecbern-swiss-premium-v16-uniform
    eecbern-swiss-premium-v16-uniform

--- lang/ti.js
+++ lang/ti.js
@@ -1,1 +1,1 @@
-window.siteLang={code:'ti',label:'ትግርኛ'};
+window.EEC_LANG_FILES = window.EEC_LANG_FILES || {}; window.EEC_LANG_FILES["ti"] = {"name": "ትግርኛ", "menu": ["ቤት", "ብዛዕባና", "ኣገልግሎታት", "ተግባራት", "ስብከታት", "ምብጻሕ", "ቀንዲ መዝገብ", "ርኸቡና", "ምሃብ"]};
